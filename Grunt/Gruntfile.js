7a59def567d3c07d532ce2cc344efc13ce389dac
jobcrackerbah <erik@critiqueit.com>
2016-05-13 15:56:29 -0700

    adds a new task to grunt file

--- Grunt/Gruntfile.js
+++ Grunt/Gruntfile.js
@@ -10,12 +10,21 @@ module.exports = function(grunt) {
           'output/styles/main.css':'styles/main.scss'
         }
       }
+    },
+    uglify: {
+        build: {
+          files: {
+            'output/js/merge_sort.min.js': 'js/merge_sort.js'
+        }
+      }
     }
   });
   
   // loads Grunt Plugins defined in package.json file 
   grunt.loadNpmTasks('grunt-sass');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
   // defines Grunt Task 
   grunt.registerTask('default', ['sass']);
+  grunt.registerTask('default', ['uglify']);
 };
 
